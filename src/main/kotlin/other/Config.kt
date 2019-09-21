c378326265e8bb5921c5ca6252e7e053b3c62347
primys <a2uso@tuta.io>
2019-09-21 12:54:39 -0700

    Add consts for AES

--- src/main/kotlin/other/Config.kt
+++ src/main/kotlin/other/Config.kt
@@ -2,9 +2,15 @@ package io.usoamic.swapbackend.other
 
 class Config {
     companion object {
+        //DB
         const val DB_URL = LocalConfig.DB_URL
         const val DB_DRIVER = LocalConfig.DB_DRIVER
         const val DB_USER = LocalConfig.DB_USER
         const val DB_PASSWORD = LocalConfig.DB_PASSWORD
+
+        //AES
+        const val AES_METHOD = LocalConfig.AES_METHOD
+        const val AES_KEY = LocalConfig.AES_KEY
+        const val AES_IV = LocalConfig.AES_IV
     }
 }
