028fd1904546acb3d0d564d84700e74d06934cfb
primys <a2uso@tuta.io>
2019-10-19 07:31:42 -0700

    Change CHAT_ID type Int -> Long

--- src/main/kotlin/io/usoamic/swapbackend/model/Bot.kt
+++ src/main/kotlin/io/usoamic/swapbackend/model/Bot.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class Bot (
     @SerializedName("username") val USERNAME: String,
     @SerializedName("token") val TOKEN: String,
-    @SerializedName("chatId") val CHAT_ID: Int
+    @SerializedName("chatId") val CHAT_ID: Long
 )
