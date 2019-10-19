adabc4853d5dcec9b51e993d542166465a0ce72e
primys <a2uso@tuta.io>
2019-10-19 07:31:16 -0700

    Update telegrambots to 4.4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     compile("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     compile("com.google.code.gson", "gson", "2.8.5")
     compile("com.github.usoamic", "usoamic-kotlin", "v1.0.10")
-    compile("org.telegram", "telegrambots", "4.4.0")
+    compile("org.telegram", "telegrambots", "4.4.0.1")
 }
 
 val fatJar = task("fatJar", type = Jar::class) {
